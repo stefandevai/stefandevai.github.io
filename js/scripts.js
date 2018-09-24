a112ac1a848b597b7c5c18581ff394a5bf13248d
Stefan Devai <stedevai@gmail.com>
2018-09-24 22:54:56 +0200

    Add reveal effect on title

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+//var showText = function (target, message, index, interval) {
+  //if (index < message.length) {
+    //$(target).append(message[index++]);
+    //setTimeout(function () {
+      //showText(target, message, index, interval);
+    //}, interval);
+  //}
+//}
+
 $(document).ready(function () {
   $('div.page-wrapper').fadeIn(1000);
 });
