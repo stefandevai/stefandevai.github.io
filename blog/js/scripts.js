119198c16d6f45a051da61858f7e0e02f41bdef4
Stefan Devai <stedevai@gmail.com>
2018-09-27 10:10:04 +0200

    Parallax and working on menu

--- blog/js/scripts.js
+++ blog/js/scripts.js
@@ -1,1 +1,15 @@
+window.addEventListener("DOMContentLoaded", parallax, false);
+
+var title = document.getElementById("blog-title");
+var poem = document.getElementById("poem");
+
+function parallax(e) {
+  applyParallax(title, -.5);
+  applyParallax(poem, .2);
+  requestAnimationFrame(parallax);
+}
+
+function applyParallax(el, speed) {
+  el.style.transform = "translate3d(0, " + window.scrollY * speed + "px, 0)";
+}
 
