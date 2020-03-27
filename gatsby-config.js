a4185f9ffe704425421e622a0f4ede75a5be511a
stefandevai <stedevai@gmail.com>
2020-03-27 04:28:37 +0100

    Add inline code highlight

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
             resolve: `gatsby-remark-prismjs`,
             options: {
               //showLineNumbers: true,
-              noInlineHighlight: true,
+              noInlineHighlight: false,
             }
           },
           {
