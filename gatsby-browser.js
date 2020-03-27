5f0b33402e6eae445d96c92581cf7556738c97f4
stefandevai <stedevai@gmail.com>
2020-03-27 04:25:57 +0100

    Add custom code highlight color theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 import "./src/styles/global.sass"
-import "prism-themes/themes/prism-duotone-dark.css"
+import "./src/styles/prism-dark-phobia-light.css"
+//import "prism-themes/themes/prism-material-light.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
