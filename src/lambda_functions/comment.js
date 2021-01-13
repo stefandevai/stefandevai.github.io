a116d44af72bac3080f8c6cacbc1c5791ddd12be
Stefan Devai <stedevai@gmail.com>
2021-01-13 10:20:54 +0100

    Add first aws lambda function

---
+++ src/lambda_functions/comment.js
@@ -0,0 +1,12 @@
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+    };
+  }
+
+  return {
+    statusCode: 200
+    body: JSON.stringify({ message: "It works!" })
+  };
+}
