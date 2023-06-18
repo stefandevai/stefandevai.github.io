ab8c4523cf4e198c3a56ba6eed42fb6cdffe3311
Stefan Devai <stefandevai@posteo.me>
2023-06-18 17:47:43 +0200

    Add footnotes to markdown

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
+import remarkFootnotes from 'remark-footnotes';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md']
+	extensions: ['.md'],
+	remarkPlugins: [remarkFootnotes],
 };
 
 /** @type {import('@sveltejs/kit').Config} */
