7620570364d5922e4e8a94c9aa185a735ecdbdd4
Stefan Devai <stefandevai@posteo.me>
2023-06-27 14:27:25 +0200

    Format code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,11 @@
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=1 />
-	<link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="1" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 <div class="layout">
