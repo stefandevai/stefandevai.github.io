74e3b2109648f1091c70c25d425561825248d4ae
Stefan Devai <stefandevai@posteo.me>
2023-07-06 15:21:11 +0200

    Change how main font is loaded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,6 @@
 
 <svelte:head>
 	<meta name="description" content="Stefan Devai's blog about History and programming." />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="1" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-		rel="stylesheet"
-	/>
 </svelte:head>
 
 <div>
