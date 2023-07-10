16d90098bb41d1415d594c72c58109e3aa31538d
Stefan Devai <stefandevai@posteo.me>
2023-07-10 20:07:56 +0200

    Add another webgl function for PSI test

--- src/routes/background.svelte
+++ src/routes/background.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-	/* import { onMount } from 'svelte'; */
-	/* import * as background from './background'; */
+	import { onMount } from 'svelte';
+	import * as background from './background';
 
 	let canvas;
 
-	/* onMount(() => { */
-	/* 	const gl = canvas.getContext('webgl'); */
+	onMount(() => {
+		const gl = canvas.getContext('webgl');
 
-	/* 	if (gl === null) { */
-	/* 		console.error('Unable to initialize WebGL. Your browser or machine may not support it.'); */
-	/* 		return; */
-	/* 	} */
+		if (gl === null) {
+			console.error('Unable to initialize WebGL. Your browser or machine may not support it.');
+			return;
+		}
 
-	/* 	background.init(gl); */
+		background.init(gl);
 
-	/* 	/1* const resizeObserver = new ResizeObserver(() => { *1/ */
-	/* 	/1* 	background.resize(gl); *1/ */
-	/* 	/1* }); *1/ */
-	/* 	/1* resizeObserver.observe(canvas); *1/ */
+		/* const resizeObserver = new ResizeObserver(() => { */
+		/* 	background.resize(gl); */
+		/* }); */
+		/* resizeObserver.observe(canvas); */
 
-	/* 	let frame; */
-	/* 	let previousTimeStamp = 0; */
+		/* let frame; */
+		/* let previousTimeStamp = 0; */
 
-	/* 	const animate = (timeStamp: number) => { */
-	/* 		if (previousTimeStamp !== timeStamp) { */
-	/* 			const delta = timeStamp - previousTimeStamp; */
-	/* 			background.animate(gl, delta); */
-	/* 		} */
+		/* const animate = (timeStamp: number) => { */
+		/* 	if (previousTimeStamp !== timeStamp) { */
+		/* 		const delta = timeStamp - previousTimeStamp; */
+		/* 		background.animate(gl, delta); */
+		/* 	} */
 
-	/* 		previousTimeStamp = timeStamp; */
-	/* 		frame = requestAnimationFrame(animate); */
-	/* 	}; */
+		/* 	previousTimeStamp = timeStamp; */
+		/* 	frame = requestAnimationFrame(animate); */
+		/* }; */
 
-	/* 	frame = requestAnimationFrame(animate); */
+		/* frame = requestAnimationFrame(animate); */
 
-	/* 	return () => { */
-	/* 		background.clear(); */
-	/* 		cancelAnimationFrame(frame); */
-	/* 		/1* resizeObserver.unobserve(canvas); *1/ */
-	/* 	}; */
-	/* }); */
+		/* return () => { */
+		/* 	background.clear(); */
+		/* 	cancelAnimationFrame(frame); */
+		/* 	/1* resizeObserver.unobserve(canvas); *1/ */
+		/* }; */
+	});
 </script>
 
 <canvas bind:this={canvas} />
