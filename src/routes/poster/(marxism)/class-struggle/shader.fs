dfe9d8f9f1c4ffecc7117b5b0c9814e715aa7642
Stefan Devai <stefandevai@posteo.me>
2023-07-25 17:37:24 +0200

    Add displacement to ray marching

--- src/routes/poster/(marxism)/class-struggle/shader.fs
+++ src/routes/poster/(marxism)/class-struggle/shader.fs
@@ -6,7 +6,36 @@ varying lowp vec4 v_position;
 uniform float u_time;
 uniform vec2 u_resolution;
 
+float circle_sdf(vec2 uv, vec2 pos, float r)
+{
+  return length(uv - pos) - r;
+}
+
 void main()
 {
-  gl_FragColor = vec4(0.,0.,0.,0.);
+  vec3 brand_color = vec3(.9,.1,.3);
+  vec2 uv = 2.*gl_FragCoord.xy/u_resolution.xy - 1.;
+  uv.x *= u_resolution.x/u_resolution.y;
+
+  float pos = .2;
+  float mult = 6.;
+
+  float d1 = circle_sdf(uv, vec2(-pos,0.), .5);
+  float d2 = circle_sdf(uv, vec2(pos,0.), .5);
+  float d3 = circle_sdf(uv, vec2(0.0,pos), .5);
+  float d4 = circle_sdf(uv, vec2(0.0,-pos), .5);
+  float color = min(d1, d2);
+  color = min(color, d3);
+  color = min(color, d4);
+
+  float pattern = color*mult - u_time*.0001;
+  pattern = mod(pattern,.5) > .25
+    ? smoothstep(.25,.27,pattern)
+    : smoothstep(.03,.01,pattern);
+
+  pattern = max(pattern, 0.8);
+
+  color = smoothstep(0., 0.01, color * pattern);
+
+  gl_FragColor = vec4(min(color, pattern) * brand_color, 1.);
 }
--- src/routes/poster/(marxism)/historical-materialism/shader.fs
+++ src/routes/poster/(marxism)/historical-materialism/shader.fs
@@ -6,10 +6,16 @@ varying lowp vec4 v_position;
 uniform float u_time;
 uniform vec2 u_resolution;
 
-const int MAX_MARCHING_STEPS = 255;
+const int MAX_MARCHING_STEPS = 55;
 const float MIN_DIST = 0.0;
 const float MAX_DIST = 100.0;
-const float EPSILON = 0.0001;
+const float EPSILON = 0.01;
+
+float smin(float a, float b, float k)
+{
+  float h = max(k-abs(a-b), 0.0)/k;
+  return min(a, b) - h*h*h*k*(1.0/6.0);
+}
 
 float cube_sdf(vec3 p)
 {
@@ -25,10 +31,40 @@ float sphere_sdf(vec3 p, float radius)
   return length(p) - radius;
 }
 
+
+float union_sdf(float a, float b) { return smin(a, b, .2); }
+
+float intersect_sdf(float a, float b) { return max(a, b); }
+
+float difference_sdf(float a, float b) { return max(a, -b); }
+
+
+float octahedron_sdf(vec3 p, float s)
+{
+  p = abs(p);
+  return (p.x+p.y+p.z-s)*0.57735027;
+}
+
+float displacement(vec3 p)
+{
+  float t = u_time * 0.001;
+  float fs = 5.;
+  float f = .21;
+  return sin(fs*p.x + t) * sin(fs*p.z + t*2.) * sin(fs*p.y) * f;
+  /* return sin(5.*p.x + time)*sin(5.*p.y + time)*cos(5.*p.z + time)*time*0.1; */
+  /* return sin(2.5*p.x)*sin(2.5*p.y - time)*cos(2.5*p.z + time); */
+}
+
 float scene_sdf(vec3 p)
 {
-  /* return sphere_sdf(p, 1.0); */
-  return cube_sdf(p - vec3(2.5,1.,0.));
+  p = vec3(p.x - 2.5, p.y - .5, p.z);
+  float sphere = sphere_sdf(p, 1.3);
+  float cube = cube_sdf(p + vec3(0.,0. + sin(u_time * 0.001),0.));
+
+  /* return union_sdf(cube, sphere); */
+  return sphere_sdf(p, 1.3) + displacement(p);
+  /* return cube_sdf(p) + displacement(p); */
+  /* return octahedron_sdf(p, 1.0) + displacement(p); */
 }
 
 vec3 ray_direction(float fov, vec2 size, vec2 frag_coord)
@@ -93,7 +129,8 @@ vec3 phong_contribution(vec3 diffuse, vec3 specular, float alpha, vec3 p, vec3 e
     return light_intensity * (diffuse * dot_LN);
   }
 
-  return light_intensity * (diffuse * dot_LN + specular * pow(dot_RN, alpha));
+  /* return light_intensity * (diffuse * dot_LN + specular * pow(dot_RN, alpha)); */
+  return light_intensity * (diffuse * dot_LN);
 }
 
 vec3 phong_illumination(vec3 ambient, vec3 diffuse, vec3 specular, float alpha, vec3 p, vec3 eye)
@@ -124,7 +161,7 @@ mat4 view_matrix(vec3 eye, vec3 center, vec3 up)
 
 void main()
 {
-  vec3 eye = vec3(8.,5.,9.);
+  vec3 eye = vec3(8.,5.,12.);
   vec3 dir = ray_direction(45., u_resolution.xy, gl_FragCoord.xy);
 
   mat4 view_to_world = view_matrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
@@ -134,15 +171,15 @@ void main()
 
   if (dist > MAX_DIST - EPSILON)
   {
-    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.);
+    gl_FragColor = vec4(0.,0.,0.,0.);
     return;
   }
 
   vec3 p = eye + dist * world_dir;
-  vec3 ambient = vec3(.2,.2,.2);
-  vec3 diffuse = vec3(.9,.3,.6);
+  vec3 ambient = vec3(0.851,0.824,0.792) * .8;
+  vec3 diffuse = vec3(0.129,0.757,0.435);
   vec3 specular = vec3(1.);
-  float shininess = 10.;
+  float shininess = 12.;
 
   vec3 color = phong_illumination(ambient, diffuse, specular, shininess, p, eye);
 
