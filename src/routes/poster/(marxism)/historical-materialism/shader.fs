98b9483c04be82a0d80a63ffe3b1ed7d81529608
Stefan Devai <stefandevai@posteo.me>
2023-07-31 21:48:31 +0200

    Add rotation to historical materialism polygons

--- src/routes/poster/(marxism)/historical-materialism/shader.fs
+++ src/routes/poster/(marxism)/historical-materialism/shader.fs
@@ -88,7 +88,8 @@ float displacement(vec3 p)
   /* return sin(2.5*p.x)*sin(2.5*p.y - time)*cos(2.5*p.z + time); */
 }
 
-float quartic_in_out(float t) {
+float exp_easing(float t)
+{
   return t == 0.0 || t == 1.0
     ? t
     : t < 0.5
@@ -96,71 +97,143 @@ float quartic_in_out(float t) {
       : -0.5 * pow(2.0, 7.0 - (t * 14.0)) + 1.0;
 }
 
+float quadratic_in_out(float t)
+{
+  float p = 2.0 * t * t;
+  return t < 0.5 ? p : -p + (4.0 * t) - 1.0;
+}
+
+mat4 rotate_y(float theta)
+{
+    float c = cos(theta);
+    float s = sin(theta);
+
+    return mat4(
+        vec4(c, 0, s, 0),
+        vec4(0, 1, 0, 0),
+        vec4(-s, 0, c, 0),
+        vec4(0, 0, 0, 1)
+    );
+}
+
+// https://github.com/glslify/glsl-inverse/blob/master/index.glsl
+mat4 inverse(mat4 m) {
+  float a00 = m[0][0], a01 = m[0][1], a02 = m[0][2], a03 = m[0][3],
+      a10 = m[1][0], a11 = m[1][1], a12 = m[1][2], a13 = m[1][3],
+      a20 = m[2][0], a21 = m[2][1], a22 = m[2][2], a23 = m[2][3],
+      a30 = m[3][0], a31 = m[3][1], a32 = m[3][2], a33 = m[3][3],
+
+      b00 = a00 * a11 - a01 * a10,
+      b01 = a00 * a12 - a02 * a10,
+      b02 = a00 * a13 - a03 * a10,
+      b03 = a01 * a12 - a02 * a11,
+      b04 = a01 * a13 - a03 * a11,
+      b05 = a02 * a13 - a03 * a12,
+      b06 = a20 * a31 - a21 * a30,
+      b07 = a20 * a32 - a22 * a30,
+      b08 = a20 * a33 - a23 * a30,
+      b09 = a21 * a32 - a22 * a31,
+      b10 = a21 * a33 - a23 * a31,
+      b11 = a22 * a33 - a23 * a32,
+
+      det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+  return mat4(
+      a11 * b11 - a12 * b10 + a13 * b09,
+      a02 * b10 - a01 * b11 - a03 * b09,
+      a31 * b05 - a32 * b04 + a33 * b03,
+      a22 * b04 - a21 * b05 - a23 * b03,
+      a12 * b08 - a10 * b11 - a13 * b07,
+      a00 * b11 - a02 * b08 + a03 * b07,
+      a32 * b02 - a30 * b05 - a33 * b01,
+      a20 * b05 - a22 * b02 + a23 * b01,
+      a10 * b10 - a11 * b08 + a13 * b06,
+      a01 * b08 - a00 * b10 - a03 * b06,
+      a30 * b04 - a31 * b02 + a33 * b00,
+      a21 * b02 - a20 * b04 - a23 * b00,
+      a11 * b07 - a10 * b09 - a12 * b06,
+      a00 * b09 - a01 * b07 + a02 * b06,
+      a31 * b01 - a30 * b03 - a32 * b00,
+      a20 * b03 - a21 * b01 + a22 * b00) / det;
+}
+
 float scene_sdf(vec3 p)
 {
-  p = vec3(p.x - 2.5, p.y - .5, p.z);
-  /* /1* float sphere = sphere_sdf(p, 1.3); *1/ */
-  /* /1* float cube = cube_sdf(p + vec3(0.,0. + sin(u_time * 0.001),0.), 1.); *1/ */
+  p = vec3(p.x - 2.9, p.y + quadratic_in_out(sin(u_time*.0015)*.3 + .5)*1.2 - 1.1, p.z);
+  p = (inverse(rotate_y(u_time*.001)) * vec4(p, 1.)).xyz;
 
-  /* /1* return union_sdf(cube, sphere); *1/ */
-  /* /1* return sphere_sdf(p, 1.3) + displacement(p); *1/ */
+  /* float sphere = sphere_sdf(p, 1.3); */
+  /* float cube = cube_sdf(p + vec3(0.,0. + sin(u_time * 0.001),0.), 1.); */
 
-  /* float num_shapes = 6.; */
-  /* float single_scene_time = 1000.; */
-  /* float transition_delay = 500.; */
-  /* float total_time = single_scene_time * num_shapes + transition_delay * num_shapes; */
-  /* float mt = mod(u_time, total_time); */
+  /* return union_sdf(cube, sphere); */
+  /* return sphere_sdf(p, 1.3); */
 
-  /* int mid = int(floor((mt) / (single_scene_time + transition_delay))); */
-  /* float bound1 = float(mid+1) * single_scene_time + float(mid) * transition_delay; */
-  /* float bound2 = bound1 + transition_delay; */
+  float num_shapes = 6.;
+  float single_scene_time = 2000.;
+  float transition_delay = 500.;
+  float total_time = single_scene_time * num_shapes + transition_delay * num_shapes;
+  float mt = mod(u_time, total_time);
 
-  /* // Morphing easing function */ 
-  /* /1* float mf = smoothstep(bound1, bound2, mt); *1/ */
-  /* float mf = quartic_in_out(max(0., (mt - bound1)/transition_delay)); */
+  int mid = int(floor((mt) / (single_scene_time + transition_delay)));
+  float bound1 = float(mid+1) * single_scene_time + float(mid) * transition_delay;
+  float bound2 = bound1 + transition_delay;
 
-  /* float shape1, shape2; */
+  // Morphing easing function 
+  /* float mf = smoothstep(bound1, bound2, mt); */
+  float mf = exp_easing(max(0., (mt - bound1)/transition_delay));
 
-  /* if (mid == 0) */
-  /* { */
-  /*   shape1 = sphere_sdf(p, 1.3); */
-  /*   shape2 = tetrahedron_sdf(p, .9); */
-  /* } */
-  /* else if (mid == 1) */
-  /* { */
-  /*   shape1 = tetrahedron_sdf(p, .9); */
-  /*   shape2 = cube_sdf(p, .9); */
-  /* } */
-  /* else if (mid == 2) */
-  /* { */
-  /*   shape1 = cube_sdf(p, .9); */
-  /*   shape2 = octahedron_sdf(p, 1.3); */
-  /* } */
-  /* else if (mid == 3) */
-  /* { */
-  /*   shape1 = octahedron_sdf(p, 1.3); */
-  /*   shape2 = dodecahedron_sdf(p, 1.1); */
-  /* } */
-  /* else if (mid == 4) */
-  /* { */
-  /*   shape1 = dodecahedron_sdf(p, 1.1); */
-  /*   shape2 = icosahedron_sdf(p, 1.2); */
-  /* } */
-  /* else */
-  /* { */
-  /*   shape1 = icosahedron_sdf(p, 1.2); */
-  /*   shape2 = sphere_sdf(p, 1.3); */
-  /* } */
+  float shape1, shape2;
 
-  /* /1* return mix(shape1, shape2, mf) + displacement(p); *1/ */
-  /* return mix(shape1, shape2, mf); */
+  if (mid == 0)
+  {
+    shape1 = sphere_sdf(p, 1.3);
+    shape2 = tetrahedron_sdf(p, .9);
+  }
+  else if (mid == 1)
+  {
+    shape1 = tetrahedron_sdf(p, .9);
+    shape2 = cube_sdf(p, .9);
+  }
+  else if (mid == 2)
+  {
+    shape1 = cube_sdf(p, .9);
+    shape2 = octahedron_sdf(p, 1.3);
+  }
+  else if (mid == 3)
+  {
+    shape1 = octahedron_sdf(p, 1.3);
+    shape2 = dodecahedron_sdf(p, 1.1);
+  }
+  else if (mid == 4)
+  {
+    shape1 = dodecahedron_sdf(p, 1.1);
+    shape2 = icosahedron_sdf(p, 1.2);
+  }
+  else
+  {
+    shape1 = icosahedron_sdf(p, 1.2);
+    shape2 = sphere_sdf(p, 1.3);
+  }
+
+  /* return mix(shape1, shape2, mf) + displacement(p); */
+  return mix(shape1, shape2, mf);
 
 
   // ======================================
   // Particles
   // ======================================
 
-  return sphere_sdf(p, 1.3);
+  /* const float num_particles = 2.; */
+  /* float total = sphere_sdf(p, 1.3); */
+
+  /* float time = u_time*.001; */
+
+  /* for (float i = 0.; i < num_particles; i += 1.) */
+  /* { */
+  /*   total = union_sdf(total, sphere_sdf(p + vec3(sin(time*1.3 + 476.*i)*2., cos(time + 987.*i)*2., .0), .1)); */
+  /* } */
+
+  /* return total; */
 }
 
 vec3 ray_direction(float fov, vec2 size, vec2 frag_coord)
@@ -250,12 +323,13 @@ mat4 view_matrix(vec3 eye, vec3 center, vec3 up)
 void main()
 {
   vec3 eye = vec3(8.,5.,12.);
+  /* vec3 eye = vec3(1.,1.,15.); */
   vec3 dir = ray_direction(45., u_resolution.xy, gl_FragCoord.xy);
 
   mat4 view_to_world = view_matrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
   vec3 world_dir = (view_to_world * vec4(dir, 0.)).xyz;
 
-  float dist =  shortest_distance(eye, world_dir, MIN_DIST, MAX_DIST);
+  float dist = shortest_distance(eye, world_dir, MIN_DIST, MAX_DIST);
 
   /* // Shiny edges */
   /* float aaf = fwidth(dist); */
